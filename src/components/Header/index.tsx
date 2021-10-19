d20acb208a4c005e097892bda429b45109db24ce
hhimanshu <dev.atwork@yahoo.com>
2021-10-19 07:31:24 -0700

    getting storybook to render svg

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import {Box, Icon, Text} from "@chakra-ui/react"
 import Logo from "../../images/svg/logo.svg"
 
 const Header = () => {
-    return <Box h={24} border={'1px solid black'}>
-        <Text fontSize={"5xl"} color={'black'}><Icon as={Logo} height={'50'} w={'auto'} mx={5}/>TheBigLogo</Text>
+    return <Box h={56} border={'1px solid black'}>
+        <Text fontSize={"7xl"}><Icon as={Logo} height={'50'} w={'auto'} mx={5}/>TheBigLogo</Text>
     </Box>
 }
 
